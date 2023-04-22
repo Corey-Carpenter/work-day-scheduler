f5237e6bc15ba91bb5d2e66d2f34f5d9bd8db80e
Corey Carpenter <stephcorey7@gmail.com>
2023-04-22 13:56:03 -0700

    added the final hours in HTML and slightly modified js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var eventsData;
 
-//function to set the color of blocks depending on the time of day it is
 function setHourColors() {
   var now = dayjs();
 
-  for (var i = 8; i < 17; i++) {
+  for (var i = 9; i < 18; i++) {
     if (i < now.hour()) {
       $("#hour-" + i + " textarea").addClass("past");
     } else if (i == now.hour()) {
@@ -17,15 +14,48 @@ function setHourColors() {
 }};
 
 function loadStoredData() {
+  var eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
+  //sets them to blank if there's no data
+  if (!eventsData) {
+     eventsData = {
+        hour9: "",
+        hour10: "",
+        hour11: "",
+        hour12: "",
+        hour13: "",
+        hour14: "",
+        hour15: "",
+        hour16: "",
+        hour17: "",
+     }
+  }
   //TODO Load existing data from local storage
 }
 
 function handleSaveClick(event) {
+  //grab data from HTML
   var saveButton = $(event.target);
-  //TODO store thi hour's data in local storage
+  var value = hourBlock.children("textarea").val();
+  var hour = hourBlock.attr("id").split("-")[1];
+
+//modify our data object
+  eventsData["hour" + hour] = value;
+
+  //TODO store this hour's data in local storage
+  localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
 }
 
 $(function() {
   setHourColors();
   loadStoredData();
 });
+
+$(".saveBtn").on("click", handleSaveClick);
+
+
+
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+
+//function to set the color of blocks depending on the time of day it is
